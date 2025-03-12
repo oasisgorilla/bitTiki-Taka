682504acc382d1d16cfba901c75fef36350b7ad4
oasisgorilla <yongjae0522@gmail.com>
2025-03-12 11:24:46 +0900

    first commit

---
+++ contracts/BTTSwap.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+contract BTTSwap {
+    address[] public allPairs;// 컨트랙트에서 생성된 유동성 풀 주소
+    // mapping(address => mapping(address => BTTPool)) public getPair; // 특정 토큰 쌍의 유동성 풀 주소를 저장
+
+
+}
