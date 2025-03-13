2f4985f69b71045e5e4d7662ccca20cb82e97c48
oasisgorilla <yongjae0522@gmail.com>
2025-03-13 18:06:03 +0900

    LP토큰 ERC20뼈대 및 권한 설정, mint기능 추가

---
+++ contracts/BTTLiquidityToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract BTTLiquidityToken is ERC20, AccessControl {
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender); // BTTPool만 이 컨트랙트의 기능에 접근할 수 있음
+    }
+
+    function mint(address to, uint256 amount) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _mint(to, amount);
+    }
+}
