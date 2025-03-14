981991bebcc1f328a1ed64174222e95a91923cc0
oasisgorilla <yongjae0522@gmail.com>
2025-03-14 14:27:39 +0900

    테스트용 토큰 컨트랙트 추가

---
+++ contracts/BTTRandomToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract BTTRandomToken is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 100*10**decimals()); // 테스트 토큰 발행
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
